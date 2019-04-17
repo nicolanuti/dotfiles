f102b215e80cd5094bcebcdc94d70e518cf81a94
Nmegazord <nicola.nuti.studio@gmail.com>
2019-04-17 11:07:04 +0200

    fixed plugin update

--- 03-install_oh-my-zsh.sh
+++ 03-install_oh-my-zsh.sh
@@ -8,3 +8,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 # Install Powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
